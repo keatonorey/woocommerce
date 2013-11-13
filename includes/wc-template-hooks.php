5e4dc245820b829c36f12cbe56e448c5df1c347f
keatonorey <keatonorey@gmail.com>
2013-11-12 23:27:17 -0800

    Ordered hooks sequentially

--- includes/wc-template-hooks.php
+++ includes/wc-template-hooks.php
@@ -79,8 +79,8 @@ add_filter( 'loop_end', 'woocommerce_reset_loop' );
  */
 add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
 add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10 );
-add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
 add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5 );
+add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
 
 /**
  * Subcategories
